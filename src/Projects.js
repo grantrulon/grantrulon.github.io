c704c7a8d3f2cf303133524a7bb49909dce52c1d
grantrulon <rulon.grant04@yahoo.com>
2022-05-19 15:02:04 -0600

    added browser router, a couple basic routes, and created Projects component

---
+++ src/Projects.js
@@ -0,0 +1,14 @@
+
+function Projects() {
+    return (
+        <div className="text-white">
+            <ul className="flex justify-center  flex-col">
+                <li>Sem 1</li>
+                <li>Sem 2</li>
+                <li>Sem 3</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Projects;
